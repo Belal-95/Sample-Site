768c78e5df9f2a8e1cde0ac58b34634c125d4dbf
belal khanger <belal-khanger@hotmail.com>
2018-10-08 11:17:48 +0530

    commit -m "MyCommit"

--- FirstDemoAppOnGit/FirstDemoAppOnGit/App_Start/BundleConfig.cs
+++ FirstDemoAppOnGit/FirstDemoAppOnGit/App_Start/BundleConfig.cs
@@ -8,8 +8,13 @@ namespace FirstDemoAppOnGit
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
+            // this has been added
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- FirstDemoAppOnGit/FirstDemoAppOnGit/Controllers/HomeController.cs
+++ FirstDemoAppOnGit/FirstDemoAppOnGit/Controllers/HomeController.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using FirstDemoAppOnGit.Common;
+using FirstDemoAppOnGit.Models;
+
 namespace FirstDemoAppOnGit.Controllers
 {
     public class HomeController : Controller
     {
+        MyBlogEntities dbEntities = new MyBlogEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -24,6 +29,72 @@ namespace FirstDemoAppOnGit.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            List<tblAdminAccount> objAdminAccount = dbEntities.SP_CheckAdminLogin(email, Utility.Encrypt(password, "ER")).ToList();
+
+            if (objAdminAccount.Count > 0)
+            {
+                //if (dbEntities.SP_CheckUserAccountStatus(email, 1).FirstOrDefault().Value > 0)
+                //{
+                //    ViewBag.Status = "Please check your mail to activate the account!!!";
+                //    ViewBag.Activation = 1;
+                //    TempData["Email"] = email;
+                //    return View();
+                //}
+                //if (dbEntities.SP_CheckUserAccountStatus(email, 2).FirstOrDefault().Value > 0)
+                //{
+                //    ViewBag.Status = "Your Account has been cancelled. Please contact to Administration!!!";
+                //    return View();
+                //}
+
+                //Session["UserId"] = objAdminAccount[0].UserId;
+
+                //Session["FirstName"] = objAdminAccount[0].tblUserPersonalDetails.FirstOrDefault().FirstName;
+
+                Session["EmailId"] = objAdminAccount[0].Emailid;
+
+                string lastAccessedDateTime = objAdminAccount[0].LastAccessedDateTime.ToString();
+
+                DateTime currentAccessedDateTime = DateTime.Now;
+
+                if (string.IsNullOrEmpty(lastAccessedDateTime))
+                {
+                    Session["LastAccessedDateTime"] = currentAccessedDateTime.ToString();
+                }
+                else
+                {
+                    Session["LastAccessedDateTime"] = lastAccessedDateTime;
+                }
+
+                //tblUserAccountDetail objUserAccountDetail = dbEntities.tblUserAccountDetails.Find(objAdminAccount[0].UserId);
+                tblAdminAccount tblObjAdminAccount = dbEntities.tblAdminAccounts.Find(objAdminAccount[0].Emailid);
+                tblObjAdminAccount.LastAccessedDateTime = currentAccessedDateTime;
+
+                dbEntities.SaveChanges();
+                ////////////
+                //string s = Session["ToPage"].ToString();
+                //if (Session["ToPage"] == "MyReminders")
+
+                //    return RedirectToAction(s, "Home", new { area = "User" });
+                ///////////////
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Status = "Invalid Email Address or Password";
+            }
             return View();
         }
     }
